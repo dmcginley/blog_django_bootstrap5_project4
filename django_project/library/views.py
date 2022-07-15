8112085c2684c131bb2e11a7d37d71e5a501e4a5
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-07-15 03:37:27 +0100

    feat: add database and post test text to site

--- django_project/library/views.py
+++ django_project/library/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
-posts = [
-    {
-        'title': 'master',
-        'author': 'cat',
-        'age': '24',
-        'year': '12 Feb 2019',
-        'content': 'lorem dolor sit amet consectetur adipisicing elit. Explicabo suscipit qui sit autem id porro repellat soluta voluptat'
-    },
-    {
-        'title': 'shop',
-        'author': 'cat',
-        'age': '18',
-        'year': '23 Sep 2009',
-        'content': 'lorem dolor sit amet consectetur adipisicing elit.olor sit amet consectetur adipisicing elit Explicabo suscipit qui sit autem id porro repellat soluta voluptat'
-    }
-]
+# posts = [
+#     {
+#         'title': 'master',
+#         'author': 'cat',
+#         'age': '24',
+#         'year': '12 Feb 2019',
+#         'content': 'lorem dolor sit amet consectetur adipisicing elit. Explicabo suscipit qui sit autem id porro repellat soluta voluptat'
+#     },
+#     {
+#         'title': 'shop',
+#         'author': 'cat',
+#         'age': '18',
+#         'year': '23 Sep 2009',
+#         'content': 'lorem dolor sit amet consectetur adipisicing elit.olor sit amet consectetur adipisicing elit Explicabo suscipit qui sit autem id porro repellat soluta voluptat'
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all
     }
     return render(request, 'library/index.html', context)
 
