023c17a2e5aca7c64f0d5fb4c1eb2dd3da5a207e
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-07-17 21:02:11 +0100

    fix: form btn width and color

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -125,6 +125,8 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+LOGIN_REDIRECT_URL = 'library-home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
