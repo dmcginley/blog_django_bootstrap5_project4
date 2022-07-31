9dc38ab340c32005f457db82be6ab728cd3657fc
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-07-31 02:51:16 +0100

    feat: add wright post route and delete and edit

--- django_project/library/urls.py
+++ django_project/library/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
+# urlpatterns = [
+#     path('', views.home, name='library-home'),
+#     path('about/', views.about, name='library-about'),
+# ]
+
 urlpatterns = [
-    path('', views.home, name='library-home'),
+    path('', PostListView.as_view(), name='library-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='library-about'),
 ]
--- django_project/library/views.py
+++ django_project/library/views.py
@@ -1,4 +1,9 @@
+from urllib import request
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+# from django_project import users
 from .models import Post
 
 # Create your views here.
@@ -6,10 +11,65 @@ from .models import Post
 
 def home(request):
     context = {
-        'posts': Post.objects.all
+        'posts': Post.objects.all()
     }
     return render(request, 'library/index.html', context)
+
+# list views
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'library/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    # template_name = 'library/index.html'
+    # context_object_name = 'posts'
+    # ordering = ['-date_posted']
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.auther = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.auther = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.auther:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.auther:
+            return True
+        return False
 
 
 def about(request):
     return render(request, 'library/about.html', {'title': 'About'})
+
+
+# def profile(request):
+#     return render(request, 'library/profile.html', {'title': 'Profile'})
