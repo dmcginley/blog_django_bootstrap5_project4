0af8cdcb84d2a181818c4dbfad8ecebd32a79606
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-08-10 16:18:55 +0100

    fix: comment section indented and content

--- django_project/library/admin.py
+++ django_project/library/admin.py
@@ -1,8 +1,10 @@
 import imp
+from xml.etree.ElementTree import Comment
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 # models
 # add post to django admin page
 admin.site.register(Post)
+admin.site.register(Comment)
--- django_project/library/models.py
+++ django_project/library/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from users.models import Profile
 
 
 # Create your models here.
@@ -20,3 +21,20 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+# the comment section
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, related_name="comments", on_delete=models.CASCADE)
+
+    # image = object.author.profile.image.url
+
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
