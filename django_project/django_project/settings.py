8ece46301bdb589c74e3a1b77c6fb0c2265d6028
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-05 12:03:59 +0100

    feat: add static root path in settings

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # added for pagedown to work
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',  # os.path.join(BASE_DIR, 'static)
 # ]
