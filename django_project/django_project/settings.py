3d371dd1f9fef7bcd0d2093ba1d771d1f3c6b12a
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-05 13:22:00 +0100

    refactor: settings change static root location

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -136,10 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 # added for pagedown to work
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',  # os.path.join(BASE_DIR, 'static)
 # ]
