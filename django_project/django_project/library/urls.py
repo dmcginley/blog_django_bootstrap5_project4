c333e57a5e801b6cb9c3dcb185061f54fa02d275
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-07 15:21:18 +0100

    fix: add runtime file

--- django_project/django_project/library/urls.py
+++ django_project/django_project/library/urls.py
@@ -7,36 +7,19 @@ from .views import (
     CommentDeleteView, CommentDetailView
 )
 from . import views
-# urlpatterns = [
-#     path('', views.home, name='library-home'),
-#     path('about/', views.about, name='library-about'),
-# ]
 
 urlpatterns = [
     path('', PostListView.as_view(), name='library-home'),
-
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-
-    # TODO: not working -?? The current path, profiles, didn’t match any of these.
-
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-
-
     path('post/<int:post_id>/comment/<int:pk>/update',
          CommentUpdateView.as_view(), name='comment-update'),
-
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
-
     path('post/<int:post_id>/comment/<int:pk>/delete',
          CommentDeleteView.as_view(), name='comment-delete'),
-
-
     path('about/', views.about, name='library-about'),
-
     path('post/<int:pk>/add-comment',
          CommentCreateView.as_view(), name='add-comment'),
 ]
