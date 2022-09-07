d55a7f3997c813bdd511ff42985224c2c96f9f80
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-07 21:56:03 +0100

    simplify requirements

--- users_app/forms.py
+++ users_app/forms.py
@@ -1,7 +1,5 @@
 from dataclasses import field
 from importlib.resources import contents
-from pyexpat import model
-from tkinter import Widget
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
