cf1737ed95fb2904946a8b93a0d0b28bf9cc207e
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-08 01:24:52 +0100

    fix: bring back django on heroku in settings

--- techblog_project/settings.py
+++ techblog_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-# import django_on_heroku
+import django_on_heroku
 import os
 from pathlib import Path
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'markdownify',
     'storages',
 ]
