496701e119a463a1e9c73889ed0469ae3d7c78a3
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-08 18:58:03 +0100

    fix: add bootstrap cdn

--- techblog_project/settings.py
+++ techblog_project/settings.py
@@ -223,8 +223,8 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # Configure Django App for Heroku.
