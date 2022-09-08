c893076ccfe304aab334b9215399f87940665119
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-08 01:22:08 +0100

    remove pip crispy file

--- techblog_project/settings.py
+++ techblog_project/settings.py
@@ -45,8 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'django_filters',
-    # 'django_quill',
     'markdownify',
     'storages',
 ]
