54a4c0276ec5f2314df5d8fccf970cd129b4034f
Donnchadh Mc Ginley <mcginley.donn@gmail.com>
2022-09-15 10:05:05 +0100

    maint: remove old pip packages

--- techblog_project/settings.py
+++ techblog_project/settings.py
@@ -127,7 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
